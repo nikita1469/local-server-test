601f2abd4426d0a17b3a0f54e5b30e0781aaaa1f
nikita1469 <pogrebnoi.nikita2505@gmail.com>
2020-07-27 17:58:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+function req() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/people');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();   // посылаем запрос на сервер
+    request.addEventListener('load', function() {   // отслеживает стадии обработки запроса
+
+        if(request.status == 200) {
+            let data = JSON.parse(request.response);   // содержит ответ от сервера
+            console.log(data); 
+
+            data.forEach(item => {
+                let card = document.createElement('div');
+
+                card.classList.add('card');
+
+                let icon;
+
+                if(item.sex === 'male') {
+                    icon = 'icons/mars.png'
+                } else {
+                    icon = 'icons/female.png'
+                };
+
+                card.innerHTML = `
+                    <img src="${item.photo}" alt="">
+                    <div class="name">${item.name} ${item.surname}</div>
+                    <div class="sex">
+                        <img src="${icon}" alt="male">
+                    </div>
+                    <div class="age">${item.age}</div>
+                `;
+
+                document.querySelector('.app').appendChild(card);
+            });
+
+        } else {
+            console.error('Что-то пошло не так..');
+        }
+    });
+
+    this.remove();
+}
+
+document.querySelector('button').addEventListener('click', req, {'once': true});
+
+});
