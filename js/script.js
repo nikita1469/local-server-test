b1427e00690fbb4921625eb16dbf38d63cef5b88
nikita1469 <pogrebnoi.nikita2505@gmail.com>
2020-07-28 19:13:09 +0300

    POST requests with MAMP server

--- js/script.js
+++ js/script.js
@@ -1,69 +1,87 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
 
-function req() {
-    // const request = new XMLHttpRequest();
-    // request.open('GET', 'http://localhost:3000/people');
-    // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    // request.send();   // посылаем запрос на сервер
-    // request.addEventListener('load', function() {   // отслеживает стадии обработки запроса
+    function req(e) {
+        e.preventDefault();
 
-    //     if(request.status == 200) {
-    //         let data = JSON.parse(request.response);   // содержит ответ от сервера
-    //         console.log(data); 
-    //         createCards(data);
+        let person = new FormData(form);
+        person.append('id', Math.random());
 
-    //     } else {
-    //         console.error('Что-то пошло не так..');
-    //     }
-    // });
+        let obj = {};
+        person.forEach((value, key) => {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        // const request = new XMLHttpRequest();
+        // request.open('POST', 'http://localhost:3000/people');
+        // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        // request.send(json);   // посылаем запрос на сервер
+        // request.addEventListener('load', function() {   // отслеживает стадии обработки запроса
+
+        //     if(request.status == 200) {
+        //         let data = JSON.parse(request.response);   // содержит ответ от сервера
+        //         console.log(data); 
+        //         createCards(data);
+
+        //     } else {
+        //         console.error('Что-то пошло не так..');
+        //     }
+        // });
 
 
     // fetch('http://localhost:3000/people')
     //     .then(data => data.json())
-        getResource('http://localhost:3000/people')
+
+        getResource('http://localhost:3000/people', obj)
         .then(data => createCards(data))
         .catch(err => console.error(err));
 
-    this.remove();
-};
-
-document.querySelector('button').addEventListener('click', req, {'once': true});  
-
-async function getResource(url) {
-    const res = await fetch(`${url}`);
-
-    if(!res.ok) {
-        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        // this.remove();
     };
 
-    return await res.json();
-};
+    form.addEventListener('submit', (e) => req(e), {'once': true});  
 
-function createCards(resData) {
-    resData.forEach(item => {
-        let card = document.createElement('div');
+    async function getResource(url, data) {
+        const res = await fetch(`${url}`, {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(data)
+        });
 
-        card.classList.add('card');
-
-        let icon;
-
-        if(item.sex === 'male') {
-            icon = 'icons/mars.png'
-        } else {
-            icon = 'icons/female.png'
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         };
 
-        card.innerHTML = `
-            <img src="${item.photo}" alt="">
-            <div class="name">${item.name} ${item.surname}</div>
-            <div class="sex">
-                <img src="${icon}" alt="male">
-            </div>
-            <div class="age">${item.age}</div>
-        `;
+        return await res.json();
+    };
 
-        document.querySelector('.app').appendChild(card);
-    });
-};
+    function createCards(resData) {
+        resData.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('card');
+
+            let icon;
+
+            if(item.sex === 'male') {
+                icon = 'icons/mars.png'
+            } else {
+                icon = 'icons/female.png'
+            };
+
+            card.innerHTML = `
+                <img src="${item.photo}" alt="">
+                <div class="name">${item.name} ${item.surname}</div>
+                <div class="sex">
+                <img src="${icon}" alt="male">
+                </div>
+                <div class="age">${item.age}</div>
+            `;
+
+            document.querySelector('.app').appendChild(card);
+        });
+    };
 
 });
